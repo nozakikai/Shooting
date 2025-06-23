1c8223ba3fc4b73c977e2ae04182ca9b7cdf0b16
MSI\kai08 <zeal24360040@fsgcl.jp>
2025-06-23 12:02:25 +0900

    プレイヤー弾と敵の衝突判定

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -13,9 +13,9 @@ public class GameFrame extends MyFrame {
 			GameWorld.player.move();
 			movePlayBullets();
 			moveEnemies();
-
+			checkPlayerAndEnemies();
+			checkPlayerBulletsAndEnemies();
 			sleep(0.03);
-
 		}
 	}
 
@@ -39,6 +39,43 @@ public class GameFrame extends MyFrame {
 			Enemy e = GameWorld.enemies.get(i);
 			e.draw(this);
 			e.move();
+
+		}
+	}
+
+	public void checkPlayerAndEnemies() {
+		for (int i = 0; i < GameWorld.enemies.size(); i++) {
+			Enemy e = GameWorld.enemies.get(i);
+			if (Math.abs(e.x - GameWorld.player.x) <= 30 &&
+					Math.abs(e.y - GameWorld.player.y) <= 30) {
+				System.out.println("やられた！");
+				GameWorld.player.y = -1000;
+			}
+		}
+	}
+
+	public void checkPlayerBulletsAndEnemies() {
+		int i = 0;
+		while (i < GameWorld.playerBullets.size()) {
+			PlayerBullet b = GameWorld.playerBullets.get(i);
+			int j = 0;
+			int hits = 0;
+			while (j < GameWorld.enemies.size()) {
+				Enemy e = GameWorld.enemies.get(j);
+				if (Math.abs(e.x - b.x) <= 30 &&
+						Math.abs(e.y - b.y) <= 30) {
+					System.out.println("当たり！");
+					hits++;
+					GameWorld.enemies.remove(j);
+				} else {
+					j++;
+				}
+			}
+			if (hits > 0) {
+				GameWorld.playerBullets.remove(i);
+			} else {
+				i++;
+			}
 
 		}
 	}
